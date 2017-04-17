a51315fa7594ad7aa0355a53433cba68d673e061
Gaertner12 <Tobi@Tobiass-MBP.fritz.box>
2017-04-17 16:27:27 +0200

    Clickable Grid Layout

--- public/js/functions.js
+++ public/js/functions.js
@@ -5,8 +5,20 @@ function createGrid(takte) {
   for(var i=0; i < 48 ; i++){
     $('.takte').append('<div class="box"></div>');
   }
-
   var taktWidth = $('.takte').width();
-  console.log(taktWidth);
   $('.grid').width(taktWidth*takte+takte*10);
+  $('.box').each(function(i, value){
+    $(this).addClass('B'+i);
+  });
+
+
+  $('.box').click(function(){
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      $(this).html('');
+    }else{
+      $(this).addClass('active');
+      $(this).html($(this).attr('class').split(' ')[1]);
+    }
+  });
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,11 @@
 $( document ).ready(function() {
   $('.box').hover(function(){
     $(this).css('opacity','0.8');
+
   },function(){
     $(this).css('opacity','0.3');
+    if($(this).hasClass('active')){
+      $(this).css('opacity','0.6');
+    }
   });
 });
