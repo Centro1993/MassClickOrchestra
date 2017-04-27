5634930eb50690c7aef64ab45700962d277c7514
Gaertner12 <Tobi@Tobiass-MBP.fritz.box>
2017-04-27 22:19:07 +0200

    vorhänge

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
+<<<<<<< Updated upstream
 	$('.backgroundWrapp').height($(window).height());
+=======
+  $('.backgroundWrapp, .curtainLeft, .curtainRight').height($(window).height());
+  setTimeout(function(){
+      $('.curtainLeft').css('left','-330px');
+      $('.curtainRight').css('right','-330px');
+  },500);
+>>>>>>> Stashed changes
 
 	$('.box').hover(function() {
 		$(this).css('opacity', '0.9');
@@ -16,5 +24,30 @@ $(document).ready(function() {
 		var activeNotes = initialiseActiveNotes();
 		pauseResumeStartTrack(activeNotes);
 	});
+
+
+  //Drag Detection
+  var isDragging = false;
+  var xcoordStart;
+  var xcoordEnd;
+  $(".note")
+  .mousedown(function(e) {
+      isDragging = false;
+      xcoordStart = e.pageX;
+      console.log('mousedown');
+
+  })
+  .mousemove(function(e) {
+      isDragging = true;
+      xcoordEnd = e.pageX;
+   })
+  .mouseup(function() {
+      var wasDragging = isDragging;
+      console.log('mouseup');
+      isDragging = false;
+      if (!wasDragging) {
+          console.log('start: '+ xcoordStart + ' end: ' + xcoordEnd);
+      }
+  });
 
 });
