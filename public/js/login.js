2954b9c14a2849f01086d8a5b61a7940d9b98560
Gaertner12 <Tobi@Tobiass-MBP.fritz.box>
2017-07-09 14:14:55 +0200

    Smaller images

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('.backgroundWrapp').height($(window).height());
-	$('.backgroundWrapp').css('background-image','url(/images/backgroundindex.jpeg)');
+	$('.backgroundWrapp').css('background-image','url(/images/backgroundindex.jpg)');
 	url  = window.location.href;
 	if(url.indexOf('error=404') != -1){
 		$('.notFound').show();
